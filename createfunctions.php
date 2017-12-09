a82cceeca99a02bd888f7291f4b494e00fefdefa
Gibson Bennett <G-Benn@users.noreply.github.com>
2017-12-09 16:09:16 -0500

    create functions

---
+++ createfunctions.php
@@ -0,0 +1,53 @@
+<?php
+
+function createArtist($conn,$bname,$website,$origin,$members,$imgurl)
+{
+	$stmt = $conn->prepare("
+		INSERT INTO Artists (bname, website,origin,members,imgurl) 
+		VALUES(?,?,?,?,?)");
+		$stmt->bind_param('sssss',$bname,$website,$origin,$members,$imgurl);
+		if($stmt->execute())
+		{
+			
+		}
+		else{
+			print 'Artist insert failure\n';
+		}
+		$stmt->close();
+}
+
+function createVenue($conn,$vname,$vcity,$vstate,$maxcap)
+{
+	$stmt = $conn->prepare("
+		INSERT INTO Venues (vname,vcity,vstate,maxcap) 
+		VALUES(?,?,?,?)");
+		$stmt->bind_param('sssi',$vname,$vcity,$vstate,$maxcap);
+		if($stmt->execute())
+		{
+			
+		}
+		else{
+			print 'create Venue failure\n';
+		}
+		$stmt->close();
+}
+
+function createEvent($conn,$fkartist,$fkvid,$edate,$etime,$price,$head)
+{
+	$stmt = $conn->prepare("
+		INSERT INTO Events (fk_artid, fk_vid, edate, etime, price, head) 
+		VALUES(?,?,?,?,?,?)");
+		$stmt->bind_param('iissii',$fkartid,$fkvid,$edate,$etime,$price,$head);
+		if($stmt->execute())
+		{
+			
+		}
+		else{
+			print 'Event insert failure\n';
+		}
+		$stmt->close();
+	
+}
+
+
+?>
