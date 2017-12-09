c7377698390b17206dffaec795f6b6c0e8f34561
Juan <jh82@live.unc.edu>
2017-12-09 16:39:44 -0500

    account preview test

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 $('document').ready(function() {
    // var HPNSIobj = new HomePageNotSignedIn();
    // HPNSIobj.pageReady();
-	var headerObj = new Header(false);
+	var headerObj = new Header(true);
 	var mainArtistPageObj = new MainArtistPage(headerObj);
 	
 	mainArtistPageObj.pageReady();
