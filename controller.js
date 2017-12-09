1a6f45d8ec54c99299aa9ca2d77ca1cc3f58829a
Juan <jh82@live.unc.edu>
2017-12-08 23:46:03 -0500

    fix

--- controller.js
+++ controller.js
@@ -2,5 +2,5 @@ $('document').ready(function() {
    // var HPNSIobj = new HomePageNotSignedIn();
    //HPNSIobj.pageReady();
 	var mainArtistPageObj = new MainArtistPage();
-	MainArtistPage.pageReady();
+	mainArtistPageObj.pageReady();
 });
