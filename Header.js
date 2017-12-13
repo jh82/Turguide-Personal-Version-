9e97c61a3364ed4cad4faf7c7cc1b256a38c0335
Juan <jh82@live.unc.edu>
2017-12-13 09:19:56 -0500

    styling

--- Header.js
+++ Header.js
@@ -151,11 +151,11 @@ var Header = function(userSignedIn, controllerObj) {
 		var currentObj = this;
     	var loginFormDiv = '<div id="loginDiv" class="floatingDiv">\
     												<form action="">\
-    													Username:<input type="text" name="username" id="username">\
-    													Password:<input type="password" name="password" id="password">	\
+    													Username:<input type="text" name="username" id="username" class="textInput">\
+    													Password:<input type="password" name="password" id="password" class="textInput">	\
     													Remember Me<input type="checkbox">\
     													<button id="loginConfirmButton" class="header-button">Login</button>\
-														<button id="loginCancelButton" class="header-button">X</button>\
+														<button id="loginCancelButton" class="header-button cancel">X</button>\
     												</form>\
     											</div>';
     	$('header').append(loginFormDiv);
@@ -175,11 +175,11 @@ var Header = function(userSignedIn, controllerObj) {
 		var currentObj = this;
     	var loginFormDiv = '<div id="signUpDiv" class="floatingDiv">\
     												<form action="">\
-    													Username:<input type="text" name="username" id="username">\
-    													Password:<input type="password" name="password" id="password">	\
-    													Confirm Password:<input type="password" name="password" id="passwordConfirm">\
+    													Username:<input type="text" name="username" id="username" class="textInput">\
+    													Password:<input type="password" name="password" id="password" class="textInput">	\
+    													Confirm Password:<input type="password" name="password" id="passwordConfirm" class="textInput">\
     													<button id="signUpConfirmButton" class="header-button">Sign Up</button>\
-														<button id="signUpCancelButton" class="header-button">X</button>\
+														<button id="signUpCancelButton" class="header-button cancel" can>X</button>\
     												</form>\
     											</div>';
     	$('header').append(loginFormDiv);
