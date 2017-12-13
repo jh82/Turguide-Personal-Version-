5e8ecdae43ccd1f081166e7122e7a613301dd816
Gibson Bennett <G-Benn@users.noreply.github.com>
2017-12-13 04:31:52 -0500

    delete cookie on logout

--- php/logout.php
+++ php/logout.php
@@ -5,6 +5,11 @@ unset($_SESSION['username']);
 unset($_SESSION['authsalt']);
 unset($_SESSION['accid']);
 
+if (isset($_COOKIE['LOGIN_AUTH']))
+{
+	setcookie('LOGIN_AUTH', $auth_cookie_val, time()-7000000, '/', 'wwwp.cs.unc.edu', true);
+}
+
 header('Content-type: application/json');
 print(json_encode(true));
 ?>
